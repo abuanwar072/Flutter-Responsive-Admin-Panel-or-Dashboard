b408f182ebc84f82b72a45e3dc1a3166ad788437
akcauser <akcaertugrulgazi@gmail.com>
2021-06-23 23:29:41 +0300

    new condition added to child aspect ratio to fix overflow for small screen

--- lib/screens/dashboard/components/my_fields.dart
+++ lib/screens/dashboard/components/my_fields.dart
@@ -40,7 +40,7 @@ class MyFiles extends StatelessWidget {
         Responsive(
           mobile: FileInfoCardGridView(
             crossAxisCount: _size.width < 650 ? 2 : 4,
-            childAspectRatio: _size.width < 650 ? 1.3 : 1,
+            childAspectRatio: _size.width < 650 && _size.width > 350 ? 1.3 : 1,
           ),
           tablet: FileInfoCardGridView(),
           desktop: FileInfoCardGridView(
