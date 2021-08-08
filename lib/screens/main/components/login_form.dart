f1d930a190a0f28180e9905dc942669c0edc4ba7
Reniel Gonzalez <reniel9230@gmail.com>
2021-08-07 21:24:06 -0400

    Setting Up Mobile Login Screen

--- lib/screens/main/components/login_form.dart
+++ lib/screens/main/components/login_form.dart
@@ -1,4 +1,3 @@
-import 'dart:js';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
--- lib/screens/main/mobile_mode.dart
+++ lib/screens/main/mobile_mode.dart
@@ -21,13 +21,18 @@ class _MobileModeState extends State<MobileMode> {
 
     return Scaffold(
         body: SafeArea(
-            child: Container(
+            child: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(horizontal: 30),
+    child: Container(
                 color: bgColor,
                 child: Column(
                     children: [
                       Image.asset('assets/images/ic_logo.png', height: heightSize * 0.3, width: widthSize * 0.6),
-                      SingleChildScrollView(
-                        child: _formLogin(context),
+                      Text(
+                        "NewsClub",
+                        style: Theme.of(context).textTheme.headline6,
+                      ),
+                      _formLogin(context),
                           // child: LoginForm(
                           //     0.007,
                           //     0.04,
@@ -41,108 +46,145 @@ class _MobileModeState extends State<MobileMode> {
                           //     0.04,
                           //     0.032
                           // )
-                      )
+
                     ]
                 )
             )
+            ),
         )
     );
   }
 
   Widget _formLogin(BuildContext context) {
     return Column(
-      children: [
-        TextField(
-          controller: _usernameController,
-          decoration: InputDecoration(
-            hintText: 'Introduzca su nombre de usuario',
-            filled: true,
-            fillColor: secondaryColor,
-            labelStyle: TextStyle(fontSize: 12),
-            contentPadding: EdgeInsets.only(left: 30),
-            enabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.blueGrey),
-              borderRadius: BorderRadius.circular(15),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.blueGrey),
-              borderRadius: BorderRadius.circular(15),
-            ),
-          ),
-        ),
-        SizedBox(height: 30),
-        TextField(
-          controller: _passwordController,
-          decoration: InputDecoration(
-            hintText: 'Contraseña',
-            counterText: 'Olvidaste la contraseña?',
-            suffixIcon: Icon(
-              Icons.visibility_off_outlined,
-              color: Colors.grey,
-            ),
-            filled: true,
-            fillColor: secondaryColor,
-            labelStyle: TextStyle(fontSize: 12),
-            contentPadding: EdgeInsets.only(left: 30),
-            enabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.blueGrey),
-              borderRadius: BorderRadius.circular(15),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.blueGrey),
-              borderRadius: BorderRadius.circular(15),
+        children: [
+          TextField(
+            controller: _usernameController,
+            decoration: InputDecoration(
+              hintText: 'Introduzca su nombre de usuario',
+              filled: true,
+              fillColor: secondaryColor,
+              labelStyle: TextStyle(fontSize: 12),
+              contentPadding: EdgeInsets.only(left: 30),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.blueGrey),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.blueGrey),
+                borderRadius: BorderRadius.circular(15),
+              ),
             ),
           ),
-        ),
-        SizedBox(height: 40),
-        Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(30),
-            /*boxShadow: [
+          SizedBox(height: 30),
+          TextField(
+            controller: _passwordController,
+            decoration: InputDecoration(
+              hintText: 'Contraseña',
+              counterText: 'Olvidaste la contraseña?',
+              suffixIcon: Icon(
+                Icons.visibility_off_outlined,
+                color: Colors.grey,
+              ),
+              filled: true,
+              fillColor: secondaryColor,
+              labelStyle: TextStyle(fontSize: 12),
+              contentPadding: EdgeInsets.only(left: 30),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.blueGrey),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.blueGrey),
+                borderRadius: BorderRadius.circular(15),
+              ),
+            ),
+          ),
+          SizedBox(height: 40),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(30),
+              /*boxShadow: [
               BoxShadow(
                 color: Colors.deepPurple,
                 spreadRadius: 10,
                 blurRadius: 20,
               ),
             ],*/
+            ),
+            child: ElevatedButton(
+              child: Container(
+                  width: double.infinity,
+                  height: 50,
+                  child: Center(child: Text("Iniciar sesión"))),
+              onPressed: (){
+
+                if((_usernameController.text =="admin") &&(_passwordController.text =="admin")){
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => UserScreen()),
+                  );
+                }else{
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      // return object of type Dialog
+                      return AlertDialog(
+                        backgroundColor: bgColor,
+                        title: new Text("Credenciales Incorrectas"),
+                        content: new Text("Por favor chequee su nombre de usuario y contraseña"
+                            " e inténtelo nuevamente"),
+                        actions: <Widget>[
+                          // usually buttons at the bottom of the dialog
+                          new TextButton(
+                            child: new Text("Cerrar"),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                }
+              },
+              style: ElevatedButton.styleFrom(
+                primary: primaryColor,
+                onPrimary: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+              ),
+            ),
           ),
-          child: ElevatedButton(
+          SizedBox(height: 40),
+          Row(children: [
+            Expanded(
+              child: Divider(
+                color: Colors.grey[300],
+                height: 50,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Text("O contacta a los administradores"),
+            ),
+            Expanded(
+              child: Divider(
+                color: Colors.grey[400],
+                height: 50,
+              ),
+            ),
+          ]),
+          SizedBox(height: 40),
+
+          ElevatedButton(
             child: Container(
                 width: double.infinity,
                 height: 50,
-                child: Center(child: Text("Iniciar sesión"))),
-            onPressed: (){
-
-              if((_usernameController.text =="admin") &&(_passwordController.text =="admin")){
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => UserScreen()),
-                );
-              }else{
-                showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    // return object of type Dialog
-                    return AlertDialog(
-                      backgroundColor: bgColor,
-                      title: new Text("Credenciales Incorrectas"),
-                      content: new Text("Por favor chequee su nombre de usuario y contraseña"
-                          " e inténtelo nuevamente"),
-                      actions: <Widget>[
-                        // usually buttons at the bottom of the dialog
-                        new TextButton(
-                          child: new Text("Cerrar"),
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                        ),
-                      ],
-                    );
-                  },
-                );
-              }
-            },
+                child: Center(child: Text("Contactar a los administradores"))),
+            onPressed: () => print("it's pressed"),
             style: ElevatedButton.styleFrom(
               primary: primaryColor,
               onPrimary: Colors.white,
@@ -151,43 +193,7 @@ class _MobileModeState extends State<MobileMode> {
               ),
             ),
           ),
-        ),
-        SizedBox(height: 40),
-        Row(children: [
-          Expanded(
-            child: Divider(
-              color: Colors.grey[300],
-              height: 50,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Text("O contacta a los administradores"),
-          ),
-          Expanded(
-            child: Divider(
-              color: Colors.grey[400],
-              height: 50,
-            ),
-          ),
-        ]),
-        SizedBox(height: 40),
-
-        ElevatedButton(
-          child: Container(
-              width: double.infinity,
-              height: 50,
-              child: Center(child: Text("Contactar a los administradores"))),
-          onPressed: () => print("it's pressed"),
-          style: ElevatedButton.styleFrom(
-            primary: primaryColor,
-            onPrimary: Colors.white,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(15),
-            ),
-          ),
-        ),
-        /* Row(
+          /* Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             _loginWithButton(image: 'images/google.png'),
@@ -195,7 +201,9 @@ class _MobileModeState extends State<MobileMode> {
             _loginWithButton(image: 'images/facebook.png'),
           ],
         ),*/
-      ],
+        ],
+
+
     );
   }
 }
