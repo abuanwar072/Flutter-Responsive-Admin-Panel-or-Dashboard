edad4276ae26f57ea1fdd2565d0a5e273660910f
Yakoub Tarkaoui <41996403+tarkoaui@users.noreply.github.com>
2023-03-03 17:46:26 +0100

    fixed conflict error between the material MenuController class and AppBar MenuController to MenuAppController

--- lib/screens/dashboard/components/header.dart
+++ lib/screens/dashboard/components/header.dart
@@ -1,4 +1,4 @@
-import 'package:admin/controllers/MenuController.dart';
+import 'package:admin/controllers/MenuAppController.dart';
 import 'package:admin/responsive.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -18,7 +18,7 @@ class Header extends StatelessWidget {
         if (!Responsive.isDesktop(context))
           IconButton(
             icon: Icon(Icons.menu),
-            onPressed: context.read<MenuController>().controlMenu,
+            onPressed: context.read<MenuAppController>().controlMenu,
           ),
         if (!Responsive.isMobile(context))
           Text(
